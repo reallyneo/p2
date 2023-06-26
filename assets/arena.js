42267919f8a6c1279f6386aa7460f76464861997
reallyneo <naheem.watson@yale.edu>
2023-06-26 17:05:27 -0400

    Update arena.js

--- assets/arena.js
+++ assets/arena.js
@@ -217,7 +217,7 @@ function scrollf() {
 	}
 };
 
-setTimeout(() => setInterval(scrollf, z), 10000);
+setTimeout(() => setInterval(scrollf, z), 15000);
 
 function resetpage() {
 	if (window.scrollY > 45000) {
